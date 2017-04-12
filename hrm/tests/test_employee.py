bde825f7511f75fa58e671b2b6483708bfaef145
Stan Hotchner <stan.hotchner@qq.com>
2017-04-12 17:40:09 +0800

    refector exception type in test

--- hrm/tests/test_employee.py
+++ hrm/tests/test_employee.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from odoo.tests.common import TransactionCase
+from odoo.exceptions import ValidationError
+from psycopg2 import IntegrityError
 
 class TestModelA(TransactionCase):
     def setUp(self):
@@ -11,16 +13,17 @@ class TestModelA(TransactionCase):
         record = self.grade.create({'grade_code': '00B', 
                                     'name': 'capten',})
                                     
-        with self.assertRaises(Exception):
-            record = self.grade.create({'grade_code': '00B', 
+        with self.assertRaises(IntegrityError):
+            record = self.grade.create({'grade_code': '00B',
                                         'name': 'capten',})
     
     def test_grade_name_is_not_NULL(self):
-        with self.assertRaises(Exception):
+        with self.assertRaises(IntegrityError):
             record = self.grade.create({'grade_code': '00B',}) #name字段给空值
             
     def test_grade_name_is_not_empty(self):
-        record = self.grade.create({'grade_code': '00B', 
+        with self.assertRaises(ValidationError):
+            record = self.grade.create({'grade_code': '00B', 
                                         'name': '',})
         '''
         print(record.name)
